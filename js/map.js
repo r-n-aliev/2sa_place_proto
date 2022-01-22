6afb125c02fb39913c3b11ea46bd7353bfe413fc
Ruslan Aliev <raliyev@belkacar.ru>
2022-01-23 01:40:35 +0300

    change map center to the place with buildings %)

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
     new google.maps.Map(document.getElementById("map"), {
         mapId: "f9b1b1bd509cc691",
-        center: { lat: 55.83, lng: 37.89 },
-        zoom: 20,
+        center: { lat: 55.743591, lng: 37.742944 },
+        zoom: 15,
     });
 }
